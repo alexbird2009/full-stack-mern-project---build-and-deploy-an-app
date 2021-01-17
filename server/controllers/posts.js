0b0d5ad72476a79e84cf181123b0758d93edeb3e
Alex <56090574+alexbird2009@users.noreply.github.com>
2021-01-18 00:37:09 +0200

    client side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,14 +2,25 @@ import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessage = await postMessage.find();
-    } catch (error) {
+        const postMessages = await PostMessage.find();
 
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message })
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('POST IS CREATED!');
-}
+export const createPost = async (req, res) => {
+    const body = req.body;
 
-// https://youtu.be/ngc9gnGgUdA?t=1545
+    const newPost = new PostMessage(post);
+    
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    
+    } catch (error) {
+        res.status(409).json({ message: error.message });        
+    }
+}
