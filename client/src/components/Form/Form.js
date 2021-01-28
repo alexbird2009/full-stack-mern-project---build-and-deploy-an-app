db97fbd4e4849d6cb004a4f7f268ba1db15d0143
unknown <ablbprk@gmail.com>
2021-01-28 11:47:13 +0200

    finished form, find errors in code, verify firebase record

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,9 +7,7 @@ import useStyles from './styles.js'
 import { createPost } from '../../actions/posts.js';
 
 const Form = () => {
-    const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
-    })
+    const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''})
     
     const classes = useStyles();
     const dispatch = useDispatch();
@@ -21,13 +19,13 @@ const Form = () => {
     }
 
     const clear = () => {
-        return(console.log('clear'));
+
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
+                <Typography variant="h6">Creating a Memory</Typography>
                 <TextField name='creator' variant='outlined' label='Creator' fullWidth value={ postData.creator } onChange={ (e) => setPostData({ ...postData, creator: e.target.value })}/>
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={ postData.title } onChange={ (e) => setPostData({ ...postData, title: e.target.value })}/>
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={ postData.message } onChange={ (e) => setPostData({ ...postData, message: e.target.value })}/>
